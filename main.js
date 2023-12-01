bb440a4d3f53173a5b3c2cfed1adbe7b28820658
Islam Sulaiman <137811809+IslamSulaiman4@users.noreply.github.com>
2023-12-01 14:11:11 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -4,15 +4,26 @@ var coursePrice= document.querySelector("#coursePrice");
 var courseDescription= document.querySelector("#courseDescription");
 var courseCapacity= document.querySelector("#courseCapacity");
 var addBtn=document.querySelector("#click");
-console.log(courseName);
 var courses=[];
+var search=document.querySelector("#search");
 var inputs=document.querySelectorAll(".inputs");
+var nameError =document.querySelector('.nameError')
+var isNameTrue=false;
+if(courses=JSON.parse(localStorage.getItem("courses"))==null){
+    var courses=[];
+}
+else{
+courses=JSON.parse(localStorage.getItem("courses"));
+
+displayData();
+}
+
 addBtn.addEventListener("click",function(e){
 e.preventDefault();
 addCourse();
 clearInputs();
 displayData();
-})
+});
 
 function addCourse(){
     var course={
@@ -23,8 +34,13 @@ function addCourse(){
         capacity:courseCapacity.value,
     }
     courses.push(course);
-    console.log(courses);
-
+    localStorage.setItem("courses",JSON.stringify(courses));
+    swal({
+        title: "Done!",
+        text: "course added successfully!",
+        icon: "success",
+        button: "OK!",
+      });
 }
 
 function clearInputs(){
@@ -54,7 +70,77 @@ document.getElementById("data").innerHTML=result;
 }
 
 function deleteCource(id){
-    courses.splice(id,1);
-    displayData();
-}
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover this!",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+            courses.splice(id,1);
+            localStorage.setItem("courses",JSON.stringify(courses));
 
+            displayData();
+            icon: "success",
+          swal("Done! Your file has been deleted!", {       
+          });
+        } else {
+          swal("Your file is safe!");
+        }
+      });}
+
+
+search.addEventListener("keyup",function(e){
+var result="";
+for(var i=0;i<courses.length;i++){
+    if(courses[i].name.toLowerCase().includes(e.target.value.toLowerCase()))
+    result+=`
+    <tr>
+    <td>${i}</td>
+    <td>${courses[i].name}</td>
+    <td>${courses[i].category}</td>
+    <td>${courses[i].price}</td>
+    <td>${courses[i].description}</td>
+    <td>${courses[i].capacity}</td>
+    <td><button class="btn btn-outline-info">Update</button></td>
+    <td><button class="btn btn-outline-danger" onclick="deleteCource(${i})">Delete</button></td>
+
+    </tr>
+    `
+}
+document.getElementById("data").innerHTML=result;
+
+})
+
+courseName.addEventListener("keyup", function(){
+    var pattern=/^[A-Z][a-z]{2,10}$/;
+    if(pattern.test(courseName.value)){
+        if(courseName.classList.contains('is-invalid')){
+            courseName.classList.remove('is-invalid');
+        }
+        courseName.classList.add('is-valid');
+        nameError.style.cssText="display:none";
+        isNameTrue=true;
+        enabled(isNameTrue);
+    }
+    else{
+        if( courseName.classList.contains('is-valid')){
+            courseName.classList.remove('is-valid');
+
+        }
+        courseName.classList.add('is-invalid');
+        nameError.style.cssText="display:block";
+        isNameTrue=false;
+        enabled(isNameTrue);
+    }
+})
+function enabled(x){
+    if(x){
+        addBtn.removeAttribute('disabled');
+    }
+    else{
+        addBtn.setAttribute('disabled', 'disabled');
+    }
+}
